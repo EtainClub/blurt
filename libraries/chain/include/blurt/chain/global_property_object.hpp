9e17b83683390b6231a15ac6edfe3131699ce90e
Tuan Pham Anh <baabeetaa@gmail.com>
2020-06-02 03:02:07 +0700

    remove BLURT_DELEGATION_RETURN_PERIOD_HF0

--- libraries/chain/include/blurt/chain/global_property_object.hpp
+++ libraries/chain/include/blurt/chain/global_property_object.hpp
@@ -97,7 +97,7 @@ namespace blurt { namespace chain {
           */
          uint32_t vote_power_reserve_rate = BLURT_REDUCED_VOTE_POWER_RATE;
 
-         uint32_t delegation_return_period = BLURT_DELEGATION_RETURN_PERIOD_HF0;
+         uint32_t delegation_return_period = BLURT_DELEGATION_RETURN_PERIOD_HF20;
 
          uint64_t reverse_auction_seconds = 0;
 
