eae54b02ad5b73a46a0258f2d605965fc3015464
Jacob Gadikian <jacobgadikian@gmail.com>
2020-06-28 04:47:51 +0000

    update bash: folder names, user creation, todos

--- doc/witness-full-node.bash
+++ doc/witness-full-node.bash
@@ -1,13 +1,19 @@
 #!/bin/bash
+# TODO: run as another user, security, system settings
+# POST-LAUNCH TODO: This should probably not be reading files directly from git, or if so, from versioned releases. 
 
 # UPDATE YOUR DEBIAN 10 SYSTEM TO THE LATEST VERSIONS OF PACKAGES
 apt update
 apt upgrade -y
 
+# CREATE BLURT USER
+useradd -m -d /blurt blurt
+
 # INSTALL UNZIP AND WGET
 apt install -y unzip wget libbz2-dev libsnappy-dev libncurses5-dev libreadline-dev
 
 # DOWNLOAD BUILD ARTIFACTS
+# POST-LAUNCH TODO: THIS SHOULD GET SOME KIND OF "LATEST" VERSION.  
 wget https://gitlab.com/blurt/blurt/-/jobs/596005137/artifacts/download
 
 # UNZIP THE BUILD ARTIFACTS, BLURTD AND CLI_WALLET
@@ -23,10 +29,9 @@ rm download
 chmod +x /usr/bin/blurtd
 chmod +x /usr/bin/cli_wallet
 
-# MAKE ~/.blurtd AND FILL IT WITH 1.3 MILLION STEEM ACCOUNTS
-mkdir /blurtd
-wget -O /blurtd/snapshot.json https://test.blurt.world/_download/snapshot.json
-wget -O /blurtd/config.ini https://gitlab.com/blurt/blurt/-/raw/dev/doc/witness_config.ini
+# IMPORT 1.3 MILLION STEEM ACCOUNTS AND CONFIGURATION TEMPLATE
+wget -O /blurt/snapshot.json https://test.blurt.world/_download/snapshot.json
+wget -O /blurt/config.ini https://gitlab.com/blurt/blurt/-/raw/dev/doc/witness_config.ini
 
 # INSTALL BLURTD.SERVICE 
 wget -O /etc/systemd/system/blurtd.service https://gitlab.com/blurt/blurt/-/raw/dev/doc/blurtd.service
@@ -36,6 +41,3 @@ systemctl enable blurtd
 
 # START BLURTD
 systemctl start blurtd 
-
-# TODO: run as another user, security, system settings
-
