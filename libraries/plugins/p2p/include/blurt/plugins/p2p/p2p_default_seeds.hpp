dda9ec96b36f074aa8b83b65b8a85ec2f6161b3f
Tuan Pham Anh <baabeetaa@gmail.com>
2020-07-14 13:59:44 +0700

    add default seed-nodes

--- libraries/plugins/p2p/include/blurt/plugins/p2p/p2p_default_seeds.hpp
+++ libraries/plugins/p2p/include/blurt/plugins/p2p/p2p_default_seeds.hpp
@@ -8,7 +8,9 @@ namespace blurt { namespace plugins { namespace p2p {
 const std::vector< std::string > default_seeds;
 #else
 const std::vector< std::string > default_seeds = {
-
+   "seed1.blurt.world:2001",
+   "seed2.blurt.world:2001",
+   "blurtseed-se.privex.io:2015"
 };
 #endif
 
